1923a7545774824d35cf50b9b4d8d1139d6b8b30
Juan Cid <jucid@softserveinc.com>
2023-09-08 00:25:11 -0300

    git ignore added

--- export_envs.sh
+++ export_envs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Export the current directory path to PYTHONPATH
-export OPENAI_API_KEY="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"
+export OPENAI_API_KEY="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"
